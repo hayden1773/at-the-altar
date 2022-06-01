7eca329ea3a3775a08d193aed0db161fe36cbc19
Seval Cakir <sevalc@live.com>
2022-06-01 11:39:11 -0700

    basic routes for models and index.js added

---
+++ routes/venueRoutes.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const { Venue, User, City, Country } = require("../models/");
+
+//find all
+router.get("/", async (req, res) => {
+  try {
+    const venueList = await Venue.findAll();
+    res.status(200).json(venueList);
+  } catch (err) {
+    res.status(500).json({ msg: "an error occured", err });
+  }
+});
+
+//find by id
+router.get("/:id", (req, res) => {
+  Venue.findByPk(req.params.id, {
+    include: [{ model: User }],
+  })
+    .then((venueData) => {
+      res.json(venueData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ msg: "an error occured", err });
+    });
+});
+
+module.exports = router;
